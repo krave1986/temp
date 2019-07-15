ea707a96a8b62f06d1bd2a3ae24c396e382edcbf
Krave Zheng <16416541+krave1986@users.noreply.github.com>
2019-07-15 23:02:24 +0800

    select测试

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,52 @@
 <template>
-  <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png" />
-    <HelloWorld msg="Welcome to Your Vue.js App" />
-  </div>
+	<div id="app">
+		<label for="catalogs">规则集：</label>
+		<select id="catalogs" v-model="bbc">
+			<optgroup label="A">
+				<option value="1">ppp</option>
+				<option value="2">qqq</option>
+			</optgroup>
+			<optgroup label="B">
+				<option value="3">ppp</option>
+				<option value="4">qqq</option>
+			</optgroup>
+			<optgroup label="C">
+				<option value="5">ppp</option>
+				<option value="6">qqq</option>
+			</optgroup>
+		</select>
+	</div>
 </template>
 
 <script>
-import HelloWorld from "./components/HelloWorld.vue";
-
 export default {
-  name: "app",
-  components: {
-    HelloWorld
-  }
+	name: "app",
+	data() {
+		return {
+			bbc: "fff"
+		};
+	},
+	watch: {
+		bbc: function(nv) {
+			console.log(nv);
+		}
+	},
+	mounted() {
+		console.log(this.bbc);
+	}
 };
 </script>
 
 <style>
-#app {
-  font-family: "Avenir", Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
+body {
+	margin: 0;
+}
+select {
+	appearance: none;
+	width: 200px;
+	height: 32px;
+	border-radius: 4px;
+	text-indent: 4px;
+	border: 1px solid rgba(217, 217, 217, 1);
 }
 </style>
